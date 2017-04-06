d4f612a5e1f8916271963de35545d002bd2f8b5b
Liliia_Sydorenko <lilya.sidorenko1@gmail.com>
2017-04-06 18:37:12 +0300

    Test commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,63 @@
 var path = require('path');
-var webpack = require('webpack');
+const webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
+    context: __dirname + '/frontend',
 
     entry: {
-        files: ['./scss/bubble.scss', './js/bubble.js']
+        home: ['./js/calculator', './scss/add-list.scss']
     },
     output: {
-        filename: "js/main.min.js",
-        path: __dirname + '/build',
+        path: __dirname + '/public',
+        filename: 'js/calculator.js'
     },
+    watch: NODE_ENV == 'development',
+
 
     module: {
-
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: [
-                    "babel-loader",
-                    "eslint-loader",
-                ],
+                loader: 'babel-loader',
+                query: {presets: ['es2015']}
             },
+            // {
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     loader: 'eslint-loader',
+            // },
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }),
+                loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', "sass-loader"]}),
             },
-
-
         ]
     },
-
-    plugins: [
-        new ExtractTextPlugin('css/styles.min.css'),
-        new UglifyJSPlugin({
-            compress: { warnings: false },
-            include: /\.min\.js$/
-        }),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.min\.css$/,
-            cssProcessorOptions: { discardComments: { removeAll: true } }
-        })
-    ],
 
     stats: {
         colors: true
     },
-    devtool: 'source-map'
+    plugins: [
+        new ExtractTextPlugin('css/styles.css'),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        }),
+
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        port: 9000,
+        watchContentBase: true,
+    },
+    devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
+
 };
+
+if (NODE_ENV == 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    )
+}
